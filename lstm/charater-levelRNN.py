9324e1e3bf48c8c96ef24eab1ca39eb4272ce0a9
Yukesh432 <nsiwansh@gmail.com>
2023-07-25 13:07:36 +0545

    characterRNN

---
+++ lstm/charater-levelRNN.py
@@ -0,0 +1,15 @@
+from io import open
+import glob
+import os
+import unicodedata
+import string
+
+
+all_letters= string.ascii_letters + ".,;"
+
+n_letters= len(all_letters)
+
+print(all_letters)
+print(n_letters)
+
+
