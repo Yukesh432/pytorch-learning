c17dac6e67b34445a9c6f4f923d54a03c949ffa1
Yukesh432 <nsiwansh@gmail.com>
2023-07-30 05:36:04 +0545

    laded pretrained model

--- CNN/objectDetection.py
+++ CNN/objectDetection.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 from PIL import Image
 from torch.utils.data import Dataset
 import torch
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 
 class PennFudanDataset(Dataset):
     def __init__(self, root, transforms):
@@ -74,3 +76,19 @@ class PennFudanDataset(Dataset):
     def __len__(self):
         # returns the total number of images in the dataset
         return len(self.imgs)
+    
+
+
+# Finetuning from pretrained model.............
+
+# loading model pre-trained on COCO
+model= torchvision.models.detection.fasterrcnn_resnet50_fpn(weights= "DEFAULT")
+# replacing the classifier with a new one, that has num_classes which is user-defined
+num_classes= 2   # 1 class(person) + background
+# get no. of input features for the classifier
+in_features= model.roi_heads.box_predictor.cls_score.in_features
+# replace the pretraiined head with a new one
+model.roi_heads.box_predictor= FastRCNNPredictor(in_features, num_classes)
+
+
+
