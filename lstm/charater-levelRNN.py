bd39c2681e9646e214c7f20f69ff7ae33a5a66c0
Yukesh432 <nsiwansh@gmail.com>
2023-07-31 21:49:42 +0545

    update:defined RNN class

--- lstm/charater-levelRNN.py
+++ lstm/charater-levelRNN.py
@@ -4,6 +4,9 @@ import os
 import unicodedata
 import string
 
+import torch
+import torch.nn as nn
+
 
 all_letters= string.ascii_letters + ".,;"
 
@@ -13,3 +16,15 @@ print(all_letters)
 print(n_letters)
 
 
+#Define RNN module class
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+        self.hidden_size= hidden_size
+        self.i2h= nn.Linear(n_categories +input_size + hidden_size, hidden_size)
+        self.i20= nn.Linear(n_categories + input_size + hidden_size, output_size)
+        self.o2o= nn.Linear(hidden_size + output_size, output_size)
+        self.dropout= nn.Dropout(0.1)
+        self.softmax= nn.LogSoftmax(dim=1)
+        
