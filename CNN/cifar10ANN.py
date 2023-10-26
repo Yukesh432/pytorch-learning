04907eb425cf3a5097b307e261e61673ed2cc86c
Yukesh432 <nsiwansh@gmail.com>
2023-10-26 20:36:58 +0545

    used ANN

---
+++ CNN/cifar10ANN.py
@@ -0,0 +1,88 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import numpy as np
+import time
+
+# Define an ANN model
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(3 * 32 * 32, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+
+    def forward(self, x):
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+# Load and preprocess the CIFAR-10 dataset
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
+
+# Define a loss function and optimizer
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+net = Net()
+net.to(device)
+print(device, "................................")
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+if __name__ == '__main__':
+    training_losses = []
+    
+    # Record the start time
+    start_time = time.time()
+
+    for epoch in range(5):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            optimizer.zero_grad()
+
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 2000 == 1999:
+                print(f'[{epoch + 1}, {i + 1}] loss: {running_loss / 2000:.3f}')
+                training_losses.append(running_loss / 2000)
+                running_loss = 0.0
+
+    print('Finished Training')
+
+
+    # Record the end time
+    end_time = time.time()
+    training_duration = end_time - start_time
+
+    print(f'Finished Training in {training_duration:.2f} seconds')
+    plt.plot(training_losses)
+    plt.title('Training Loss Over Time')
+    plt.xlabel('Mini-Batch Iterations')
+    plt.ylabel('Loss')
+    plt.show()
+
+    # You can save the trained model
+    # torch.save(net.state_dict(), 'cifar10_net_ann.pth')
