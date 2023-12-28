b0ce8882e48e2d197ad0ce047c1c6c913bbf3667
Yukesh432 <nsiwansh@gmail.com>
2023-12-28 23:29:06 +0545

    eval

--- CNN/fire-detection/main.py
+++ CNN/fire-detection/main.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from data_loader import load_data
-from model import train_model, initialize_model
+from model import train_model, initialize_model, evaluate_model, plot_confusion_matrix
 
 def main():
-    data_dir = 'C:/Users/AIXI/OneDrive/Desktop/projects/fire-detection/Fire-Detection'
+    data_dir = 'C:/Users/AIXI/OneDrive/Desktop/projects/pytorch-learning/CNN/data/Fire-Detection'
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print("devide is ...................", device)
     
@@ -15,10 +15,15 @@ def main():
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    trained_model = train_model(model, dataloaders, criterion, optimizer, num_epochs=25, device=device)
+    trained_model = train_model(model, dataloaders, criterion, optimizer, num_epochs=5, device=device)
+
+    # After training, evaluate the model
+    conf_matrix, class_report = evaluate_model(model, dataloaders, device)
+    print("Classification Report:\n", class_report)
+    plot_confusion_matrix(conf_matrix)
 
     # Save the model
-    torch.save(trained_model.state_dict(), 'fire_detection_model.pth')
+    torch.save(trained_model.state_dict(), 'fire_detection_model2.pth')
 
 if __name__ == "__main__":
     main()
