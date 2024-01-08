fff1251be8e06ed858eb867ddbfcc86f3a9a0f7f
Yukesh432 <nsiwansh@gmail.com>
2024-01-08 22:01:50 +0545

    evaluation

--- lstm/model.py
+++ lstm/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import TensorDataset, DataLoader
-
+import math
 # LSTM Model Definition
 class TokenLSTM(nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_size, num_layers=1):
@@ -66,3 +66,21 @@ def train_model(model, data_loader, learning_rate, num_epochs, device):
 
         average_loss = total_loss / len(data_loader)
         print(f'Epoch [{epoch+1}/{num_epochs}], Average Loss: {average_loss}')
+
+def evaluate_model(model, data_loader, device):
+    model.eval()  # Set the model to evaluation mode
+    criterion = nn.CrossEntropyLoss()
+    total_loss = 0
+    total_count = 0
+
+    with torch.no_grad():
+        for inputs, targets in data_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs.view(-1, model.vocab_size), targets.view(-1))
+            total_loss += loss.item() * inputs.size(0)
+            total_count += inputs.size(0)
+
+    average_loss = total_loss / total_count
+    perplexity = math.exp(average_loss)
+    return perplexity
