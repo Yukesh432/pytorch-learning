5d111740a9a59925bdfb6d8f895a72874a49aafc
Yukesh432 <nsiwansh@gmail.com>
2024-02-26 16:00:03 +0545

    update:character level rnn

--- lstm/character-level-rnn/inference.py
+++ lstm/character-level-rnn/inference.py
@@ -26,17 +26,18 @@ def generate_text(model, start_prompt, char_to_int, int_to_char, n_vocab, genera
 def load_checkpoint(model, checkpoint_path):
     """
     Loads a model checkpoint.
-    """
+    """ 
     checkpoint = torch.load(checkpoint_path)
     model.load_state_dict(checkpoint['model_state_dict'])
     return model
 
-def inference_from_checkpoints(checkpoint_dir, char_to_int, int_to_char, n_vocab, start_prompt, generate_length=100):
+def inference_from_checkpoints(checkpoint_dir, char_to_int, int_to_char, n_vocab, start_prompt, generate_length=50):
     """
     Perform inference using multiple model checkpoints.
     """
     # List your checkpoint files here. Assuming they are named with a pattern like 'model_checkpoint_epoch_{epoch}.pth'
-    checkpoint_files = [f"{checkpoint_dir}_epoch_{epoch}.pth" for epoch in range(5, 31, 5)]  # Adjust range as needed
+    # checkpoint_files = [f"{checkpoint_dir}_epoch_{epoch}.pth" for epoch in range(5, 11, 5)]  # Adjust range as needed
+    checkpoint_files = [f"{checkpoint_dir}_epoch_{epoch}.pth" for epoch in range(5, 51, 5)]  # Adjust range as needed
 
     for checkpoint_path in checkpoint_files:
         model = CharModel(1, 256, 2, n_vocab, 0.2)  # Ensure this matches your model's architecture
@@ -50,10 +51,11 @@ def main():
     dataX, dataY, char_to_int, n_vocab, _ = load_data(filename, seq_length)
     int_to_char = {i: c for c, i in char_to_int.items()}
     
-    start_prompt = "i want to"  # Example prompt
+    # start_prompt = "Alice noticed with "  # Example prompt
+    start_prompt = "if she were looking over their shoulders"  # Example prompt
     checkpoint_dir = "model_checkpoint"  # Adjust as needed
 
-    inference_from_checkpoints(checkpoint_dir, char_to_int, int_to_char, n_vocab, start_prompt, 100)  # Generate 100 characters
+    inference_from_checkpoints(checkpoint_dir, char_to_int, int_to_char, n_vocab, start_prompt, 50)  # Generate 100 characters
 
 if __name__ == "__main__":
     main()
