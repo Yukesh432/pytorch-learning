346a1b707d6bddc664956f8f2c4efd7448c9e95f
Yukesh432 <nsiwansh@gmail.com>
2024-05-15 23:53:01 +0545

    building helper function

---
+++ RNN/main.py
@@ -0,0 +1,11 @@
+import io
+import os
+import unicodedata
+import string
+import torchimport random
+
+ALL_LETTERS= string.ascii_letters+ ",.;"
+NUM_LETTERS= len(ALL_LETTERS)
+
+print(NUM_LETTERS)
+print(ALL_LETTERS)
